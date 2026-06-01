beca028fdb766b8a648c4f963fc600519274d45a
Kwon06 <jw0614jw0614@gmail.com>
2026-06-01 16:33:57 +0900

    kitchen.h

--- code/c_queue/kitchen.h
+++ code/c_queue/kitchen.h
@@ -3,35 +3,75 @@
 
 #include "types.h"
 #include "queue.h"
+#include "kitchen.h"
 
-// =========================================================
-// [김지원 담당] 주방 큐 및 조리 알고리즘 헤더 파일 (kitchen.h)
-// =========================================================
+// c : 새로 들어온 손님
+void Kitchen_Enqueue_FIFO(Queue* kitchen_queue, Customer* c) // 시나리오1[선입선출] 함수
+{
+    Enqueue(kitchen_queue, c);
+}
+==================================================================================
+void Kitchen_Enqueue_Priority(Queue* kitchen_queue, Customer* c) // 시나리오2[최단 작업 우선] 함수
+{
+    Node* newNode = (Node*)malloc(sizeof(Node));
 
-/*
- * 1. [시나리오 1] 기본 FIFO 주방 큐 삽입 
- * - 역할: 키오스크에서 넘어온 주문을 무조건 큐의 맨 뒤에 입력
- * - 이 함수는 queue.c에 있는 Enqueue()를 그대로 호출하면 될 거 같아요
- */
-void Kitchen_Enqueue_FIFO(Queue* kitchen_queue, Customer* c);
+    newNode->data = c;
+    newNode->next = NULL;
 
-/*
- * 2. [시나리오 2] 최단 작업 우선(SJF) 우선순위 큐 삽입      (시나리오를 나눌 핵심 부분입니다)
- * - 역할: 들어온 순서와 상관없이 조리 시간(remaining_time)이 짧은 사람이 
- * 먼저 요리되도록 큐의 중간(알맞은 위치)에 끼워 넣고,
- * - 단순 Enqueue가 아니라, Linked List의 next 포인터를 비교하면서 
- * 들어갈 자리를 찾는 정렬 알고리즘이 필요해요
- */
-void Kitchen_Enqueue_Priority(Queue* kitchen_queue, Customer* c);
+    // 큐가 비어있는 경우
+    if (kitchen_queue->front == NULL)
+    {
+        kitchen_queue->front = newNode;
+        kitchen_queue->rear = newNode;
+        return;
+    }
 
-/*
- * 3. 주방 조리 타이머 진행 및 완료 처리 (시나리오 1, 2 공통 사용)
- * - 역할: 매 1초(Tick)마다 호출되며, 주방 큐 맨 앞 요리의 remaining_time을 -1 깎습니다.
- * - 리턴값: 시간이 0이 되어 요리가 완성된 손님의 Customer* 포인터. 
- * (아직 조리 중이거나 큐가 비어있으면 NULL 반환)
- * - 요리가 완성되면 Dequeue()로 빼서 리턴부탁 드립니다.
- * 그러면 제가(이충한) 받아서 손님을 퇴장시킬게요
- */
-Customer* Kitchen_Process_Tick(Queue* kitchen_queue);
+    // 맨 앞에 들어가야 하는 경우
+    if (c->remaining_time <
+        kitchen_queue->front->data->remaining_time)
+    {
+        newNode->next = kitchen_queue->front;
+        kitchen_queue->front = newNode;
+        return;
+    }
+
+    Node* current = kitchen_queue->front;
+
+    while (current->next != NULL &&
+           current->next->data->remaining_time <=
+           c->remaining_time)
+    {
+        current = current->next;
+    }
+
+    newNode->next = current->next;
+    current->next = newNode;
+
+    // 마지막에 삽입된 경우 rear 갱신
+    if (newNode->next == NULL)
+    {
+        kitchen_queue->rear = newNode;
+    }
+}
+==================================================================================
+Customer* Kitchen_Process_Tick(Queue* kitchen_queue) // 주방 타이머 진행/완료 처리 함수
+{
+    if (kitchen_queue->front == NULL)
+    {
+        return NULL;
+    }
+
+    Customer* current =
+        kitchen_queue->front->data;
+
+    current->remaining_time--;
+
+    if (current->remaining_time <= 0)
+    {
+        return Dequeue(kitchen_queue);
+    }
+
+    return NULL;
+}
 
 #endif
